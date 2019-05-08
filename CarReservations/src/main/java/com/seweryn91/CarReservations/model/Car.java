49044661ebde841a25266082af314c444fe34302
Seweryn91 <s.talaj@gmail.com>
2019-05-08 15:47:20 +0200

    Implement class methods

--- CarReservations/src/main/java/com/seweryn91/CarReservations/model/Car.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/model/Car.java
@@ -1,4 +1,98 @@
 package com.seweryn91.CarReservations.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
 public class Car {
+
+    @Id
+    @Column(name = "car_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long carId;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "seats")
+    private int seats;
+
+    @Column(name = "automaticGearbox")
+    private boolean automaticGearbox;
+
+    @Column(name = "automaticAC")
+    private boolean automaticAC;
+
+    public long getCarId() {
+        return carId;
+    }
+
+    public void setCarId(long carId) {
+        this.carId = carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public boolean isAutomaticGearbox() {
+        return automaticGearbox;
+    }
+
+    public void setAutomaticGearbox(boolean automaticGearbox) {
+        this.automaticGearbox = automaticGearbox;
+    }
+
+    public boolean isAutomaticAC() {
+        return automaticAC;
+    }
+
+    public void setAutomaticAC(boolean automaticAC) {
+        this.automaticAC = automaticAC;
+    }
 }
