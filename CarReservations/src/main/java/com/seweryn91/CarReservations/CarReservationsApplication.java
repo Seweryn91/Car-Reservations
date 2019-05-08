5eb18d0dc84f4223504cd4c75b7263f943f1532d
Seweryn91 <s.talaj@gmail.com>
2019-05-08 15:34:23 +0200

    Initial commit

---
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/CarReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.seweryn91.CarReservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarReservationsApplication.class, args);
+	}
+
+}
