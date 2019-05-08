5870b449e1e798e414f2f4adfef2e2f2568b0215
Seweryn91 <s.talaj@gmail.com>
2019-05-08 15:52:58 +0200

    Implement methods for class Customer

--- CarReservations/src/main/java/com/seweryn91/CarReservations/model/Customer.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/model/Customer.java
@@ -1,4 +1,110 @@
 package com.seweryn91.CarReservations.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @Column(name = "client_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long clientId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "zipcode")
+    private String zipcode;
+
+    @Column(name = "address")
+    private String address;
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
 }
