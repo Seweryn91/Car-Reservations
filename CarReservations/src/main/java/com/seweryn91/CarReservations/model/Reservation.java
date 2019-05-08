185c11f02604e3acfdeb047aa494c17a43a067e9
Seweryn91 <s.talaj@gmail.com>
2019-05-08 15:57:49 +0200

    Implement methods for Reservation class

--- CarReservations/src/main/java/com/seweryn91/CarReservations/model/Reservation.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/model/Reservation.java
@@ -1,4 +1,66 @@
 package com.seweryn91.CarReservations.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "reservation")
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "reservation_id")
+    private long reservationId;
+
+    @Column(name = "car_id")
+    private long carId;
+
+    @Column(name = "client_id")
+    private long clientId;
+
+    @Column(name = "reservation_start")
+    private Date startDate;
+
+    @Column(name = "reservation_end")
+    private Date endDate;
+
+    public long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public long getCarId() {
+        return carId;
+    }
+
+    public void setCarId(long carId) {
+        this.carId = carId;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
