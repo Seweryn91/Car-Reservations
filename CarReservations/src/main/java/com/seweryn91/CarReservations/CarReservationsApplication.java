a1bbd37dd13431234e996b34d0c8e466f9136ab6
Seweryn91 <s.talaj@gmail.com>
2019-05-08 15:44:10 +0200

    Reindent file

--- CarReservations/src/main/java/com/seweryn91/CarReservations/CarReservationsApplication.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/CarReservationsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarReservationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarReservationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarReservationsApplication.class, args);
+    }
 
 }
