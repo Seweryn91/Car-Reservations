af7e071511ff7ed7ab32f649a654962f4d3a76a5
Seweryn91 <s.talaj@gmail.com>
2019-05-21 16:19:57 +0200

    Implement method updateReservation

--- CarReservations/src/main/java/com/seweryn91/CarReservations/dao/ReservationDAO.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/dao/ReservationDAO.java
@@ -1,39 +1,77 @@
 package com.seweryn91.CarReservations.dao;
 
-import com.seweryn91.CarReservations.database.HibernateUtil;
 import com.seweryn91.CarReservations.model.Reservation;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class ReservationDAO {
 
     private SessionFactory sessionFactory;
 
     public Reservation getReservation(long reservationId) {
-        Session session = sessionFactory.getCurrentSession();
-        Reservation reservation = session.get(Reservation.class, reservationId);
+        Transaction tx = null;
+        Reservation reservation = null;
+        try {
+            Session session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            reservation = session.get(Reservation.class, reservationId);
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
         return reservation;
     }
 
 
     public void saveReservation(Reservation reservation) {
-        Session session = sessionFactory.getCurrentSession();
-        Reservation reservationFromDB = session.get(Reservation.class, reservation.getReservationId());
-        if (reservationFromDB == null) {
-            session.save(reservation);
-        } else {
-            reservation = reservationFromDB;
+        Transaction tx = null;
+        try {
+            Session session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            Reservation reservationFromDB = session.get(Reservation.class, reservation.getReservationId());
+            if (reservationFromDB == null) {
+                session.save(reservation);
+            } else {
+                reservation = reservationFromDB;
+            }
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
         }
     }
 
 
     public void deleteReservation(long reservationId) {
+        Transaction tx = null;
+        try {
+            Session session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            Reservation reservationInDB = session.load(Reservation.class, reservationId);
+            if (reservationInDB != null) {
+                session.delete(reservationId);
+            }
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
 
-        Session session = sessionFactory.getCurrentSession();
-        Reservation reservationInDB = session.load(Reservation.class, reservationId);
-        if (reservationInDB != null) {
-            session.delete(reservationId);
+    public void updateReservation(Reservation reservation) {
+        long reservationId = reservation.getReservationId();
+        Transaction tx = null;
+        try {
+            Session session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            Reservation reservationInDB = session.get(Reservation.class, reservationId);
+            if (reservationInDB != null) {
+                session.update(reservation);
+            }
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
         }
     }
 }
