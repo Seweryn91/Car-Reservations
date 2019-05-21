7b2a8d7aeee48df627a125637c137d08217fda0a
Seweryn91 <s.talaj@gmail.com>
2019-05-21 15:07:12 +0200

    Change annotation to Component

--- CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CarDAO.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CarDAO.java
@@ -5,14 +5,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
-
-import javax.transaction.Transactional;
 import java.util.List;
 
-@Repository
-@Transactional
+@Component
 public class CarDAO {
 
     @Autowired
