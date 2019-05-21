20f131efd925f0691539d817b7a2ed30fa1aa4a2
Seweryn91 <s.talaj@gmail.com>
2019-05-21 15:11:23 +0200

    Implement method saveCustomer

--- CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CustomerDAO.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CustomerDAO.java
@@ -1,60 +1,28 @@
 package com.seweryn91.CarReservations.dao;
 
-import com.seweryn91.CarReservations.database.ConnectionDriver;
+import com.seweryn91.CarReservations.database.HibernateUtil;
 import com.seweryn91.CarReservations.model.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
 
-import java.sql.*;
-
+@Component
 public class CustomerDAO {
 
-    void addCustomer(Customer customer) throws SQLException {
+    private SessionFactory sessionFactory;
 
-        String query = "INSERT INTO customer VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING;";
-
-        try (
-                Connection connection = ConnectionDriver.getConnection();
-                PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)
-                ) {
-
-            statement.setString(1, customer.getFirstName());
-            statement.setString(2, customer.getLastName());
-            statement.setString(3, customer.getEmail());
-            statement.setString(4, customer.getPhone());
-            statement.setString(5, customer.getCountry());
-            statement.setString(6, customer.getCity());
-            statement.setString(7, customer.getZipcode());
-            statement.setString(8, customer.getZipcode());
-
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
-                throw new SQLException("Adding customer to database failed, no rows affected! \n" +
-                        "Prossibly user already exists or email was used before");
-            }
-            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    customer.setCustomerId(generatedKeys.getLong(1));
-                } else {
-                    throw new SQLException("Inserting customer failed, no ID obtained!");
-                }
-            }
-
+    public void saveCustomer(Customer customer) {
+        Transaction tx = null;
+        try {
+            Session session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            session.save(customer);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
         }
     }
 
-    public void deleteCustomer(Customer customer) throws SQLException {
-
-        String query = "DELETE FROM customer WHERE customer_id = ?";
-
-        try (
-                Connection connection = ConnectionDriver.getConnection();
-                PreparedStatement statement = connection.prepareStatement(query);
-        ) {
-            statement.setLong(1, customer.getCustomerId());
-
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
-                throw new SQLException("Deleting customer failed, no rows affected!");
-            }
-        }
-    }
 }
