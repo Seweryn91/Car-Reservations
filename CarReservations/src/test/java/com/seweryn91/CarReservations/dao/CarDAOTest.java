65df8fe6e10e546a00076d4e8ca0191f1d03544b
Seweryn91 <s.talaj@gmail.com>
2019-06-03 16:08:50 +0200

    Rename methods, add 'test-' prefix

--- CarReservations/src/test/java/com/seweryn91/CarReservations/dao/CarDAOTest.java
+++ CarReservations/src/test/java/com/seweryn91/CarReservations/dao/CarDAOTest.java
@@ -1,51 +1,83 @@
 package com.seweryn91.CarReservations.dao;
 
 import com.seweryn91.CarReservations.model.Car;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @SpringBootTest
+@Transactional
 class CarDAOTest {
 
     @Autowired
             CarDAO carDAO;
+    SessionFactory sessionFactory;
 
+    //@BeforeAll
+    void setup() {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(Car.class)
+            .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+            .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+            .setProperty("username", "postgres").setProperty("password", "postgres")
+            .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/carreservations?currentSchema=public&user=postgres&password=postgres")
+            .setProperty("hibernate.hbm2ddl.auto", "none");
+
+        sessionFactory = configuration.buildSessionFactory();
+        sessionFactory.openSession();
+        }
+
+    @DisplayName("Test get car from DB")
     @Test
-    void getCar() {
+    void testGetCar() {
         Assertions.assertNotNull(carDAO.getCar(1));
     }
 
+    @DisplayName("Test save car in DB")
     @Test
-    void saveCar() {
-        Car car = setup();
+    void testSaveCar() {
+        setup();
+        Car car = createCar();
         carDAO.saveCar(car);
+        List<Car> testCarList = carDAO.getCarsOfCategory("Crap");
+        Car carFromList = testCarList.get(0);
+        Assertions.assertEquals(car.getBrand(), carFromList.getBrand());
+        Assertions.assertEquals(car.getModel(), carFromList.getModel());
+        Assertions.assertEquals(car.getCategory(), carFromList.getCategory());
+        Assertions.assertEquals(car.getPrice(), carFromList.getPrice());
+        Assertions.assertEquals(car.getDoors(), carFromList.getDoors());
+        Assertions.assertEquals(car.getYear(), carFromList.getYear());
+    }
+
+    @Test
+    void testFindAllCars() {
 
     }
 
     @Test
-    void findAllCars() {
-
+    void testGetCarsOfCategory() {
     }
 
     @Test
-    void getCarsOfCategory() {
+    void testDeleteCar() {
     }
 
     @Test
-    void deleteCar() {
+    void testGetCarById() {
     }
 
     @Test
-    void getCarById() {
+    void testUpdateCar() {
     }
 
-    @Test
-    void updateCar() {
-    }
-
-    Car setup() {
+    Car createCar() {
         Car car = new Car();
         car.setBrand("Lada");
         car.setModel("2105");
