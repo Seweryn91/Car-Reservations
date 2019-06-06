711ee1a07871a4c468d01ed13b8760c849695134
Seweryn91 <s.talaj@gmail.com>
2019-06-06 16:09:12 +0200

    Implement method testGetCustomer

--- CarReservations/src/test/java/com/seweryn91/CarReservations/dao/CustomerDAOTest.java
+++ CarReservations/src/test/java/com/seweryn91/CarReservations/dao/CustomerDAOTest.java
@@ -1,9 +1,21 @@
 package com.seweryn91.CarReservations.dao;
 
 import com.seweryn91.CarReservations.model.Customer;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
 class CustomerDAOTest {
+
+    @Autowired
+    CustomerDAO customerDAO;
+    SessionFactory sessionFactory;
 
     @Test
     void testSaveCustomer() {
@@ -11,6 +23,10 @@ class CustomerDAOTest {
 
     @Test
     void testGetCustomer() {
+        Customer customer = customerDAO.getCustomer(1);
+        Assertions.assertEquals("Aegon II", customer.getFirstName());
+        Assertions.assertEquals("Targaryen", customer.getLastName());
+        Assertions.assertEquals("Seven Kingdoms", customer.getCountry());
     }
 
     @Test
@@ -53,5 +69,16 @@ class CustomerDAOTest {
     void testUpdateCustomerAddress() {
     }
 
-    Customer createCustomer() {}
+    Customer createCustomer() {
+        Customer customer = new Customer();
+        customer.setFirstName("Jane");
+        customer.setLastName("Dough");
+        customer.setEmail("jennie@fakemail.com");
+        customer.setPhone("0123456");
+        customer.setCountry("Neveland");
+        customer.setCity("Village Town City");
+        customer.setZipcode("11-231");
+        customer.setAddress("00 Seven St.");
+        return customer;
+    }
 }
