01c22e31a2937b891757906df96e0752e4f07b2d
Seweryn91 <s.talaj@gmail.com>
2019-06-06 15:15:35 +0200

    Create class CustomerDAOTest

---
+++ CarReservations/src/test/java/com/seweryn91/CarReservations/dao/CustomerDAOTest.java
@@ -0,0 +1,57 @@
+package com.seweryn91.CarReservations.dao;
+
+import com.seweryn91.CarReservations.model.Customer;
+import org.junit.jupiter.api.Test;
+
+class CustomerDAOTest {
+
+    @Test
+    void testSaveCustomer() {
+    }
+
+    @Test
+    void testGetCustomer() {
+    }
+
+    @Test
+    void testFindAllCustomers() {
+    }
+
+    @Test
+    void testDeleteCustomer() {
+    }
+
+    @Test
+    void testUpdateCustomerFirstName() {
+    }
+
+    @Test
+    void testUpdateCustomerLastName() {
+    }
+
+    @Test
+    void testUpdateCustomerEmail() {
+    }
+
+    @Test
+    void testUpdateCustomerPhone() {
+    }
+
+    @Test
+    void testUpdateCustomerCountry() {
+    }
+
+    @Test
+    void testUpdateCustomerCity() {
+    }
+
+    @Test
+    void testUpdateCustomerZipcode() {
+    }
+
+    @Test
+    void testUpdateCustomerAddress() {
+    }
+
+    Customer createCustomer() {}
+}
