afa326fbe86bd6879c57eb26adb97f72ce13f4a1
Seweryn91 <s.talaj@gmail.com>
2019-06-07 18:07:40 +0200

    Refactor class, add methods for updating date, car, and customer

--- CarReservations/src/main/java/com/seweryn91/CarReservations/dao/ReservationDAO.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/dao/ReservationDAO.java
@@ -6,6 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -16,10 +17,9 @@ public class ReservationDAO {
     public Reservation getReservation(long reservationId) {
         Transaction tx = null;
         Reservation reservation = null;
-        try {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             tx = session.beginTransaction();
-            reservation = session.get(Reservation.class, reservationId);
+            reservation = session.byId(Reservation.class).load(reservationId);
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             e.printStackTrace();
@@ -30,15 +30,9 @@ public class ReservationDAO {
 
     public void saveReservation(Reservation reservation) {
         Transaction tx = null;
-        try {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             tx = session.beginTransaction();
-            Reservation reservationFromDB = session.get(Reservation.class, reservation.getReservationId());
-            if (reservationFromDB == null) {
-                session.save(reservation);
-            } else {
-                reservation = reservationFromDB;
-            }
+            session.save(reservation);
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             e.printStackTrace();
@@ -48,29 +42,62 @@ public class ReservationDAO {
 
     public void deleteReservation(long reservationId) {
         Transaction tx = null;
-        try {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             tx = session.beginTransaction();
-            Reservation reservationInDB = session.load(Reservation.class, reservationId);
-            if (reservationInDB != null) {
-                session.delete(reservationId);
-            }
+            Reservation reservation = getReservation(reservationId);
+            session.delete(reservation);
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             e.printStackTrace();
         }
     }
 
-    public void updateReservation(Reservation reservation) {
-        long reservationId = reservation.getReservationId();
+    public void updateReservationCar(long reservationId, long carId) {
         Transaction tx = null;
-        try {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             tx = session.beginTransaction();
-            Reservation reservationInDB = session.get(Reservation.class, reservationId);
-            if (reservationInDB != null) {
-                session.update(reservation);
-            }
+            Reservation reservation = getReservation(reservationId);
+            reservation.setCarId(carId);
+            session.update(reservation);
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void updateResrvationCustomer(long reservationId, long customerId) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            Reservation reservation = getReservation(reservationId);
+            reservation.setCustomerId(customerId);
+            session.update(reservation);
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void updateReservationStart(long reservationId, Date startDate) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            Reservation reservation = getReservation(reservationId);
+            reservation.setStartDate(startDate);
+            session.update(reservation);
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void updateReservationEnd(long reservationId, Date endDate) {
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            Reservation reservation = getReservation(reservationId);
+            reservation.setEndDate(endDate);
+            session.update(reservation);
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             e.printStackTrace();
@@ -78,11 +105,10 @@ public class ReservationDAO {
     }
 
     @SuppressWarnings("unchecked")
-    public List<Reservation> getAllReservations(Reservation reservation) {
+    public List<Reservation> getAllReservations() {
         Transaction tx = null;
         List<Reservation> allReservations = null;
-        try {
-            Session session = sessionFactory.openSession();
+        try (Session session = sessionFactory.openSession()) {
             tx = session.beginTransaction();
             allReservations = (List<Reservation>) session.createQuery("from Reservation").list();
             tx.commit();
