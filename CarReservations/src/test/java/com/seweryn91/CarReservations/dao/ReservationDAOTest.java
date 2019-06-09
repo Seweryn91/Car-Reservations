df3d88ebe23ba070e67c5ad8288725b3654692dc
Seweryn91 <s.talaj@gmail.com>
2019-06-09 14:24:17 +0200

    Create class ReservationDAOTest

---
+++ CarReservations/src/test/java/com/seweryn91/CarReservations/dao/ReservationDAOTest.java
@@ -0,0 +1,40 @@
+package com.seweryn91.CarReservations.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationDAOTest {
+
+    @Test
+    void testGetReservation() {
+    }
+
+    @Test
+    void testSaveReservation() {
+    }
+
+    @Test
+    void testDeleteReservation() {
+    }
+
+    @Test
+    void testUpdateReservationCar() {
+    }
+
+    @Test
+    void testUpdateReservationCustomer() {
+    }
+
+    @Test
+    void testUpdateReservationStart() {
+    }
+
+    @Test
+    void testUpdateReservationEnd() {
+    }
+
+    @Test
+    void testGetAllReservations() {
+    }
+}
