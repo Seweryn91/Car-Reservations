ae6d0961abf86e6ed08bbfb9fe3fa7ddb43eb01a
Seweryn91 <s.talaj@gmail.com>
2019-06-09 14:47:31 +0200

    Implement method testGetReservation

--- CarReservations/src/test/java/com/seweryn91/CarReservations/dao/ReservationDAOTest.java
+++ CarReservations/src/test/java/com/seweryn91/CarReservations/dao/ReservationDAOTest.java
@@ -1,13 +1,27 @@
 package com.seweryn91.CarReservations.dao;
 
+import com.seweryn91.CarReservations.model.Reservation;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.transaction.Transactional;
 
+@SpringBootTest
+@Transactional
 class ReservationDAOTest {
+
+    @Autowired
+    ReservationDAO reservationDAO;
+    SessionFactory sessionFactory;
 
     @Test
     void testGetReservation() {
+        Reservation reservation = reservationDAO.getReservation(1);
+        Assertions.assertEquals(2, reservation.getCustomerId());
+        Assertions.assertEquals(9, reservation.getCarId());
     }
 
     @Test
