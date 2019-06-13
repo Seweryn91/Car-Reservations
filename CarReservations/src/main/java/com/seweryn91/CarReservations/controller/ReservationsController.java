d97b1a49a399f3b633827f9e0df8bcf37eb90662
Seweryn91 <s.talaj@gmail.com>
2019-06-13 17:55:41 +0200

    Refactor method getAllReservations

--- CarReservations/src/main/java/com/seweryn91/CarReservations/controller/ReservationsController.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/controller/ReservationsController.java
@@ -2,23 +2,36 @@ package com.seweryn91.CarReservations.controller;
 
 import com.seweryn91.CarReservations.dao.ReservationDAO;
 import com.seweryn91.CarReservations.model.Reservation;
+import com.seweryn91.CarReservations.utils.JSONFormatter;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
-import java.util.*;
+import java.util.List;
 
+@RestController
 public class ReservationsController {
 
+    @Autowired
     private SessionFactory sessionFactory;
+
+    @Autowired
     private ReservationDAO reservationDAO;
 
+    @Autowired
+    private JSONFormatter jsonFormatter;
+
     @RequestMapping (value = "/reservations", method = RequestMethod.GET)
-    @ResponseBody
-    private List<Reservation> getAllReservations() {
-        List<Reservation> reservations = reservationDAO.getAllReservations();
-        return reservations;
+    private String getAllReservations() {
+        StringBuilder sb = new StringBuilder();
+        try {
+            List<Reservation> reservations = reservationDAO.getAllReservations();
+            sb.append(jsonFormatter.serializeCollectionReservations(reservations));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
