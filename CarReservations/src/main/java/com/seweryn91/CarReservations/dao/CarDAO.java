2de0c975ea82830283c1fc89e4895ea1888106a4
Seweryn91 <s.talaj@gmail.com>
2019-06-13 17:26:12 +0200

    Implement method getCarsAvailableToday

--- CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CarDAO.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CarDAO.java
@@ -217,4 +217,19 @@ public class CarDAO {
         }
     }
 
+
+    @SuppressWarnings("unchecked")
+    public List<Car> getAllCarsAvailableToday() {
+        Transaction tx = null;
+        List<Car> carsAvailableToday = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            carsAvailableToday = (List<Car>) session.createQuery(" select c from Car c left join Reservation r on c.carId = r.carId where (current_date not between r.startDate and r.endDate) or (r.startDate is null) or (r.endDate is null) order by c.carId").list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+        return carsAvailableToday;
+    }
 }
