4786910a1990abc40aec4cd0e31de9149547f1de
Seweryn91 <s.talaj@gmail.com>
2019-06-14 16:02:37 +0200

    Refactor method getAllCarsAvailableToday

--- CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CarDAO.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/dao/CarDAO.java
@@ -222,9 +222,11 @@ public class CarDAO {
     public List<Car> getAllCarsAvailableToday() {
         Transaction tx = null;
         List<Car> carsAvailableToday = null;
+        String query = "select c from Car c left join Reservation r on c.carId = r.carId where (current_date not between" +
+                " r.startDate and r.endDate) or (r.startDate is null) or (r.endDate is null) order by c.carId";
         try (Session session = sessionFactory.openSession()) {
             tx = session.beginTransaction();
-            carsAvailableToday = (List<Car>) session.createQuery(" select c from Car c left join Reservation r on c.carId = r.carId where (current_date not between r.startDate and r.endDate) or (r.startDate is null) or (r.endDate is null) order by c.carId").list();
+            carsAvailableToday = (List<Car>) session.createQuery(query).list();
             tx.commit();
         } catch (Exception e) {
             if (tx != null) tx.rollback();
