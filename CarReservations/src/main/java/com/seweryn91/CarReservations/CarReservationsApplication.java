82d51994f783509ffa3d69cc4a495ae4c2e875ac
Seweryn91 <s.talaj@gmail.com>
2019-06-17 18:35:00 +0200

    Add annotations to exclude Hibernate JPA autoconfiguration and add Property source

--- CarReservations/src/main/java/com/seweryn91/CarReservations/CarReservationsApplication.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/CarReservationsApplication.java
@@ -2,8 +2,11 @@ package com.seweryn91.CarReservations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= HibernateJpaAutoConfiguration.class)
+@PropertySource("classpath:application.properties")
 public class CarReservationsApplication {
 
     public static void main(String[] args) {
