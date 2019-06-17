19684961024757811328ba75b7b54196a7107895
Seweryn91 <s.talaj@gmail.com>
2019-06-17 10:29:11 +0200

    Refactor test class, add methods before and after

--- CarReservations/src/test/java/com/seweryn91/CarReservations/dao/ReservationDAOTest.java
+++ CarReservations/src/test/java/com/seweryn91/CarReservations/dao/ReservationDAOTest.java
@@ -2,9 +2,7 @@ package com.seweryn91.CarReservations.dao;
 
 import com.seweryn91.CarReservations.model.Reservation;
 import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,123 +14,16 @@ import java.util.List;
 
 @SpringBootTest
 @Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ReservationDAOTest {
 
     @Autowired
     ReservationDAO reservationDAO;
     SessionFactory sessionFactory;
+    private long testReservationId;
 
-    @Test
-    @DisplayName("Test get reservation from DB")
-    void testGetReservation() {
-        Reservation reservation = reservationDAO.getReservation(1);
-        Assertions.assertEquals(2, reservation.getCustomerId());
-        Assertions.assertEquals(9, reservation.getCarId());
-    }
-
-    @Test
-    @DisplayName("Test save reservation in DB")
-    void testSaveReservation() {
-        List<Reservation> reservations = reservationDAO.getAllReservations();
-        int prevSize = reservations.size();
-        Reservation reservation = createReservation();
-        reservationDAO.saveReservation(reservation);
-        List<Reservation> reservationsAfterInsert = reservationDAO.getAllReservations();
-        int newSize = reservationsAfterInsert.size();
-        Assertions.assertEquals(prevSize + 1, newSize);
-        reservationDAO.deleteReservation(reservation.getReservationId());
-    }
-
-    @Test
-    @DisplayName("Test delete reservation from DB")
-    void testDeleteReservation() {
-        Reservation reservation = createReservation();
-        reservationDAO.saveReservation(reservation);
-        List<Reservation> reservations = reservationDAO.getAllReservations();
-        int prevSize = reservations.size();
-        reservationDAO.deleteReservation(reservation.getReservationId());
-        List<Reservation> reservationsAfterDelete = reservationDAO.getAllReservations();
-        int newSize = reservationsAfterDelete.size();
-        Assertions.assertEquals(prevSize - 1, newSize);
-    }
-
-    @Test
-    @DisplayName("Test update car")
-    void testUpdateReservationCar() {
-        Reservation reservation = createReservation();
-        reservationDAO.saveReservation(reservation);
-        long carId = 1;
-        reservationDAO.updateReservationCar(reservation.getReservationId(), carId);
-        Reservation afterUpdate = reservationDAO.getReservation(reservation.getReservationId());
-        long newId = afterUpdate.getCarId();
-        Assertions.assertEquals(carId, newId);
-        reservationDAO.deleteReservation(reservation.getReservationId());
-    }
-
-    @Test
-    @DisplayName("Test update customer")
-    void testUpdateReservationCustomer() {
-        Reservation reservation = createReservation();
-        reservationDAO.saveReservation(reservation);
-        long customerId = 1;
-        reservationDAO.updateReservationCustomer(reservation.getReservationId(), customerId);
-        Reservation afterUpdate = reservationDAO.getReservation(reservation.getReservationId());
-        long newId = afterUpdate.getCustomerId();
-        Assertions.assertEquals(customerId, newId);
-        reservationDAO.deleteReservation(reservation.getReservationId());
-    }
-
-    @Test
-    @DisplayName("Test update start date")
-    void testUpdateReservationStart() {
-        Reservation reservation = createReservation();
-        reservationDAO.saveReservation(reservation);
-        String pattern ="yyyy-MM-dd";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        Date startDate = null;
-        try {
-            startDate = simpleDateFormat.parse("2030-02-01");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        reservationDAO.updateReservationStart(reservation.getReservationId(), startDate);
-        Reservation afterUpdate = reservationDAO.getReservation(reservation.getReservationId());
-        String newDate = afterUpdate.getStartDate().toString();
-        Assertions.assertEquals("2030-02-01 00:00:00.0", newDate);
-        reservationDAO.deleteReservation(reservation.getReservationId());
-    }
-
-    @Test
-    @DisplayName("Test update end date")
-    void testUpdateReservationEnd() {
-        Reservation reservation = createReservation();
-        reservationDAO.saveReservation(reservation);
-        String pattern ="yyyy-MM-dd";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        Date endDate = null;
-        try {
-            endDate = simpleDateFormat.parse("2030-05-01");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        reservationDAO.updateReservationStart(reservation.getReservationId(), endDate);
-        Reservation afterUpdate = reservationDAO.getReservation(reservation.getReservationId());
-        String newDate = afterUpdate.getStartDate().toString();
-        Assertions.assertEquals("2030-05-01 00:00:00.0", newDate);
-        reservationDAO.deleteReservation(reservation.getReservationId());
-    }
-
-    @Test
-    @DisplayName("Test get all reservations")
-    void testGetAllReservations() {
-        List<Reservation> reservations = reservationDAO.getAllReservations();
-        Assertions.assertEquals(3, reservations.size());
-        Reservation reservation1 = reservations.get(0);
-        Assertions.assertEquals(9, reservation1.getCarId());
-        Assertions.assertEquals(2, reservation1.getCustomerId());
-    }
-
-    Reservation createReservation() {
+    @BeforeAll
+    void before() {
         Reservation reservation = new Reservation();
         reservation.setCustomerId(3);
         reservation.setCarId(3);
@@ -149,6 +40,137 @@ class ReservationDAOTest {
         reservation.setStartDate(startDate);
         reservation.setEndDate(endDate);
 
-        return reservation;
+        reservationDAO.saveReservation(reservation);
+        testReservationId = reservation.getReservationId();
     }
+
+    @AfterAll
+    void after() {
+        reservationDAO.deleteReservation(testReservationId);
+    }
+
+    @Test
+    @DisplayName("Test get reservation from DB")
+    void testGetReservation() {
+        Reservation reservation = reservationDAO.getReservation(1);
+        Assertions.assertEquals(2, reservation.getCustomerId());
+        Assertions.assertEquals(9, reservation.getCarId());
+    }
+
+    @Test
+    @DisplayName("Test save reservation in DB")
+    void testSaveReservation() {
+        List<Reservation> reservations = reservationDAO.getAllReservations();
+        int prevSize = reservations.size();
+        Reservation reservation = new Reservation();
+        reservation.setCustomerId(1);
+        reservation.setCarId(1);
+        String pattern ="yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            startDate = simpleDateFormat.parse("1991-01-01");
+            endDate = simpleDateFormat.parse("1991-02-01");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        reservation.setStartDate(startDate);
+        reservation.setEndDate(endDate);
+        reservationDAO.saveReservation(reservation);
+        long testReservationId2 = reservation.getReservationId();
+        List<Reservation> reservationsAfterInsert = reservationDAO.getAllReservations();
+        int newSize = reservationsAfterInsert.size();
+        Assertions.assertEquals(prevSize + 1, newSize);
+        reservationDAO.deleteReservation(testReservationId2);
+    }
+
+    @Test
+    @DisplayName("Test delete reservation from DB")
+    void testDeleteReservation() {
+        Reservation reservation = new Reservation();
+        reservation.setCustomerId(1);
+        reservation.setCarId(1);
+        String pattern ="yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            startDate = simpleDateFormat.parse("1991-01-01");
+            endDate = simpleDateFormat.parse("1991-02-01");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        reservation.setStartDate(startDate);
+        reservation.setEndDate(endDate);
+        reservationDAO.saveReservation(reservation);
+        List<Reservation> reservations = reservationDAO.getAllReservations();
+        int prevSize = reservations.size();
+        reservationDAO.deleteReservation(reservation.getReservationId());
+        List<Reservation> reservationsAfterDelete = reservationDAO.getAllReservations();
+        int newSize = reservationsAfterDelete.size();
+        Assertions.assertEquals(prevSize - 1, newSize);
+    }
+
+    @Test
+    @DisplayName("Test update car")
+    void testUpdateReservationCar() {
+        long carId = 21;
+        reservationDAO.updateReservationCar(testReservationId, carId);
+        Reservation afterUpdate = reservationDAO.getReservation(testReservationId);
+        long newId = afterUpdate.getCarId();
+        Assertions.assertEquals(carId, newId);
+    }
+
+    @Test
+    @DisplayName("Test update customer")
+    void testUpdateReservationCustomer() {
+        long customerId = 3;
+        reservationDAO.updateReservationCustomer(testReservationId, customerId);
+        Reservation afterUpdate = reservationDAO.getReservation(testReservationId);
+        long newId = afterUpdate.getCustomerId();
+        Assertions.assertEquals(customerId, newId);
+    }
+
+    @Test
+    @DisplayName("Test update start date")
+    void testUpdateReservationStart() {
+        String pattern ="yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date startDate = null;
+        try {
+            startDate = simpleDateFormat.parse("2030-02-01");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        reservationDAO.updateReservationStart(testReservationId, startDate);
+        Reservation afterUpdate = reservationDAO.getReservation(testReservationId);
+        String newDate = afterUpdate.getStartDate().toString();
+        Assertions.assertEquals("2030-02-01 00:00:00.0", newDate);
+    }
+
+    @Test
+    @DisplayName("Test update end date")
+    void testUpdateReservationEnd() {
+        String pattern ="yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date endDate = null;
+        try {
+            endDate = simpleDateFormat.parse("2030-05-01");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        reservationDAO.updateReservationEnd(testReservationId, endDate);
+        Reservation afterUpdate = reservationDAO.getReservation(testReservationId);
+        String newDate = afterUpdate.getEndDate().toString();
+        Assertions.assertEquals("2030-05-01 00:00:00.0", newDate);
+    }
+
+    @Test
+    @DisplayName("Test get all reservations")
+    void testGetAllReservations() {
+        List<Reservation> reservations = reservationDAO.getAllReservations();
+        Assertions.assertEquals(4, reservations.size());
+    }
+
 }
