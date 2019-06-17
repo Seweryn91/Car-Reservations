7ea2cd6b554d21afe1d0c02c6d000eb670bc740b
Seweryn91 <s.talaj@gmail.com>
2019-06-17 18:32:23 +0200

    Modify table car, add column for car image link

--- CarReservations/src/main/resources/static/schema.sql
+++ CarReservations/src/main/resources/static/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE car (car_id SERIAL PRIMARY KEY, brand VARCHAR(64) NOT NULL, model VARCHAR(64) NOT NULL, year INTEGER, price FLOAT8 NOT NULL, category VARCHAR(64), seats INTEGER, doors INTEGER, automaticGearbox BOOLEAN, automaticAC BOOLEAN);
+CREATE TABLE car (car_id SERIAL PRIMARY KEY, brand VARCHAR(64) NOT NULL, model VARCHAR(64) NOT NULL, year INTEGER, price FLOAT8 NOT NULL, category VARCHAR(64), seats INTEGER, doors INTEGER, automaticGearbox BOOLEAN, automaticAC BOOLEAN, imageSource TEXT DEFAULT NULL);
 
 CREATE TABLE reservation (reservation_id SERIAL PRIMARY KEY, car_id INTEGER NOT NULL, customer_id INTEGER NOT NULL, reservation_start DATE, reservation_end DATE);
 
