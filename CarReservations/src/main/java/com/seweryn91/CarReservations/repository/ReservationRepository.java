299596634e788094f278fe081d5f16f45a00ea27
Seweryn91 <s.talaj@gmail.com>
2019-07-03 13:36:45 +0200

    Add missing @Repository annotation

--- CarReservations/src/main/java/com/seweryn91/CarReservations/repository/ReservationRepository.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/repository/ReservationRepository.java
@@ -2,6 +2,8 @@ package com.seweryn91.CarReservations.repository;
 
 import com.seweryn91.CarReservations.model.Reservation;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 }
