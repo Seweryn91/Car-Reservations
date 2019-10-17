e5b3e47cb5c488e588ab7865a56eb30a05e2f5da
Seweryn91 <s.talaj@gmail.com>
2019-10-17 23:00:14 +0200

    Refactor methods, return objects instead of strings

--- CarReservations/src/main/java/com/seweryn91/CarReservations/controller/CarController.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/controller/CarController.java
@@ -3,7 +3,6 @@ package com.seweryn91.CarReservations.controller;
 import com.seweryn91.CarReservations.dao.CarDAO;
 import com.seweryn91.CarReservations.model.Car;
 import com.seweryn91.CarReservations.service.CarService;
-import com.seweryn91.CarReservations.utils.JSONFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +17,6 @@ import java.util.List;
 public class CarController {
 
     @Autowired
-    private JSONFormatter jsonFormatter;
-
-    @Autowired
     private CarDAO carDAO;
 
     @Autowired
@@ -29,102 +25,84 @@ public class CarController {
     private List<Car> cars = new ArrayList<>();
 
     @RequestMapping(value = "/cars", method = RequestMethod.GET)
-    public String getAllCars() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllCars() {
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
-        sb.append(jsonFormatter.serializeCollectionCars(cars));
-        return sb.toString();
+        return cars;
     }
 
     @RequestMapping(value = "/cars/compact", method = RequestMethod.GET)
-    public String getAllCompact() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllCompact() {
         List<Car> compacts = new ArrayList<>();
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
         cars.stream().filter(c -> c.getCategory().equals("Compact")).forEach(compacts::add);
-        sb.append(jsonFormatter.serializeCollectionCars(compacts));
-        return sb.toString();
+        return compacts;
     }
 
     @RequestMapping(value = "/cars/economic", method = RequestMethod.GET)
-    public String getAllEconomic() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllEconomic() {
         List<Car> economics = new ArrayList<>();
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
         cars.stream().filter(c -> c.getCategory().equals("Economic")).forEach(economics::add);
-        sb.append(jsonFormatter.serializeCollectionCars(economics));
-        return sb.toString();
+        return economics;
     }
 
     @RequestMapping(value = "/cars/minivan", method = RequestMethod.GET)
-    public String getAllMinivan() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllMinivan() {
         List<Car> minivans = new ArrayList<>();
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
         cars.stream().filter(c -> c.getCategory().equals("Minivan")).forEach(minivans::add);
-        sb.append(jsonFormatter.serializeCollectionCars(minivans));
-        return sb.toString();
+       return minivans;
     }
 
     @RequestMapping(value = "/cars/suv", method = RequestMethod.GET)
-    public String getAllSUV() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllSUV() {
         List<Car> suvs = new ArrayList<>();
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
         cars.stream().filter(c -> c.getCategory().equals("SUV")).forEach(suvs::add);
-        sb.append(jsonFormatter.serializeCollectionCars(suvs));
-        return sb.toString();
+        return suvs;
     }
 
     @RequestMapping(value = "/cars/family", method = RequestMethod.GET)
-    public String getAllFamily() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllFamily() {
         List<Car> families = new ArrayList<>();
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
         cars.stream().filter(c -> c.getCategory().equals("Family")).forEach(families::add);
-        sb.append(jsonFormatter.serializeCollectionCars(families));
-        return sb.toString();
+        return families;
     }
 
     @RequestMapping(value = "/cars/airconditioning", method = RequestMethod.GET)
-    public String getAllAirConditioning() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getAllAirConditioning() {
         if (cars.isEmpty()) {
             carDAO.findAllCars().forEach(cars::add);
         }
         List<Car> carsWithAC = new ArrayList<>();
         cars.stream().filter(Car::isAutomaticAC).forEach(carsWithAC::add);
-        sb.append(jsonFormatter.serializeCollectionCars(carsWithAC));
-        return sb.toString();
+        return carsWithAC;
     }
 
     @RequestMapping(value = "/cars/today")
-    public String getCarsAvailableToday() {
-        StringBuilder sb = new StringBuilder();
+    public List<Car> getCarsAvailableToday() {
         List<Car> carsAvailable = carService.getAllCarsAvailableToday();
-        sb.append(jsonFormatter.serializeCollectionCars(carsAvailable));
-        return sb.toString();
+        return carsAvailable;
     }
 
     @RequestMapping(value = "/cars/{carId}", method = RequestMethod.GET)
-    public String getCar(@PathVariable(value = "carId") Long carId) {
-        String carString = "";
+    public Car getCar(@PathVariable(value = "carId") Long carId) {
         Car car = carDAO.getCarById(carId);
         if (car == null) throw new EntityNotFoundException("Provided incorrect ID");
-        carString = jsonFormatter.serialize(car);
-        return carString;
+        return car;
     }
 
 }
--- CarReservations/src/main/java/com/seweryn91/CarReservations/controller/ReservationsController.java
+++ CarReservations/src/main/java/com/seweryn91/CarReservations/controller/ReservationsController.java
@@ -2,7 +2,6 @@ package com.seweryn91.CarReservations.controller;
 
 import com.seweryn91.CarReservations.dao.ReservationDAO;
 import com.seweryn91.CarReservations.model.Reservation;
-import com.seweryn91.CarReservations.utils.JSONFormatter;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,19 +19,11 @@ public class ReservationsController {
     @Autowired
     private ReservationDAO reservationDAO;
 
-    @Autowired
-    private JSONFormatter jsonFormatter;
 
     @RequestMapping (value = "/reservations", method = RequestMethod.GET)
-    private String getAllReservations() {
-        StringBuilder sb = new StringBuilder();
-        try {
-            List<Reservation> reservations = reservationDAO.getAllReservations();
-            sb.append(jsonFormatter.serializeCollectionReservations(reservations));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
+    private List<Reservation> getAllReservations() {
+        List<Reservation> reservations = reservationDAO.getAllReservations();
+        return reservations;
     }
 
     public Double getPrice(long reservationId) {
